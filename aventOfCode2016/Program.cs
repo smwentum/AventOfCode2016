5fac526903e2fb1ffd6319e235b3bf010d51fc59
smwentum <sai.wentum@huskers.unl.edu>
2016-12-01 21:11:18 -0500

    FIrst commit just seeing if this is working

--- aventOfCode2016/Program.cs
+++ aventOfCode2016/Program.cs
@@ -10,6 +10,8 @@ namespace aventOfCode2016
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is a sample project for the 2016 Avent of code challenge that runs from Dec 1st to Dec 25");
+            Console.WriteLine("I will try to do as many of these as i can and i will post all of my answers on the 26th");
         }
     }
 }
